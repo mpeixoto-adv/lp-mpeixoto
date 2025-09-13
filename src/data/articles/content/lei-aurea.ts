76a1e8b08492c2f8d747c65d4c0c4a5c0b1f283d
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-13 15:09:07 -0300

    Remove conteúdo: LEI AUREA

--- src/data/articles/content/lei-aurea.ts
+++
@@ -1,7 +0,0 @@
-export const articleContent = {
-  content: `<p>TESTETESTETESTETESTETESTE</p><p>TESTE</p><p>TESTE<br>TESTE<br>TESTE<br><br>TESTE</p><p></p>`,
-  metadata: {
-    lastModified: "2025-09-13T18:03:57.518Z",
-    views: 0
-  }
-}

3196ce68a30f646ad1b2605febf843fdbb36913b
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-13 20:39:54 -0300

    Atualiza conteúdo: Nova versao LGDP - EDITADO

---
+++ src/data/articles/content/nova-versao-lgdp-editado.ts
@@ -0,0 +1,7 @@
+export const articleContent = {
+  content: `<ul><li><p>Novo artigo sobre LGPD  <strong>-<em>EDITADO NOVO</em></strong></p></li></ul><p></p>`,
+  metadata: {
+    lastModified: "2025-09-13T23:39:54.294Z",
+    views: 0
+  }
+}

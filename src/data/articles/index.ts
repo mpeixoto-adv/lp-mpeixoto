4732765d428cedd93062b1db3799d58504448dc0
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-13 20:39:53 -0300

    Atualiza metadados: Nova versao LGDP - EDITADO

--- src/data/articles/index.ts
+++ src/data/articles/index.ts
@@ -5,15 +5,15 @@ import { loadArticleContentHybrid } from './loader'
 export const articlesMetadata: ArticleMetadata[] = [
   {
     id: "4",
-    title: "Nova versao LGDP",
+    title: "Nova versao LGDP - EDITADO",
     excerpt: "Novo documento sobre LGPD",
-    author: "Dr. Marcelo Peixoto",
+    author: "Dr. Marcelo Peixoto - EDITAR",
     date: "2025-09-13",
     category: "Direito Civil",
     image: "https://images.unsplash.com/photo-1450101499163-c8848c66ca85?q=80&w=2070",
     readTime: "1 min",
-    slug: "nova-versao-lgdp",
-    contentFile: "nova-versao-lgdp"
+    slug: "nova-versao-lgdp-editado",
+    contentFile: "nova-versao-lgdp-editado"
   },
   {
     id: "1",

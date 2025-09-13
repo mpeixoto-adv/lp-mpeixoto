4a454167bfa2ebbda61d0768d89990a86ac8894a
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-13 17:21:50 -0300

    Atualiza conteúdo: Teste de artigo editado

---
+++ src/data/articles/content/teste-de-artigo-editado.ts
@@ -0,0 +1,7 @@
+export const articleContent = {
+  content: `<p>Esse é um texto de input de um novo artigo editado</p>`,
+  metadata: {
+    lastModified: "2025-09-13T20:21:50.135Z",
+    views: 0
+  }
+}

42f7e68a3c07bb3e68a60d3c794d61d7828d837a
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-13 17:40:34 -0300

    Atualiza conteúdo: Teste de artigo editado de novo e de novo e DE NOVO

---
+++ src/data/articles/content/teste-de-artigo-editado-de-novo-e-de-novo-e-de-novo.ts
@@ -0,0 +1,7 @@
+export const articleContent = {
+  content: `<p>Esse é um texto de input de um novo artigo editado de novo e de novo e DE NOVO</p>`,
+  metadata: {
+    lastModified: "2025-09-13T20:40:34.625Z",
+    views: 0
+  }
+}

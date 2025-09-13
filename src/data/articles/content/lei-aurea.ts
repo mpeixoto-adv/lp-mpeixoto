e1280a65c1812c5a6690a98a063efd00db71f7e0
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-13 15:03:57 -0300

    Adiciona conteúdo: LEI AUREA

---
+++ src/data/articles/content/lei-aurea.ts
@@ -0,0 +1,7 @@
+export const articleContent = {
+  content: `<p>TESTETESTETESTETESTETESTE</p><p>TESTE</p><p>TESTE<br>TESTE<br>TESTE<br><br>TESTE</p><p></p>`,
+  metadata: {
+    lastModified: "2025-09-13T18:03:57.518Z",
+    views: 0
+  }
+}

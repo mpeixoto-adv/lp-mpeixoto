5a9d6617f0405852492782b98380b8847c3e0c48
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-13 17:43:44 -0300

    Remove conteúdo: Teste de artigo editado de novo e de novo e DE NOVO

--- src/data/articles/content/teste-de-artigo-editado-de-novo-e-de-novo-e-de-novo.ts
+++
@@ -1,7 +0,0 @@
-export const articleContent = {
-  content: `<p>Esse é um texto de input de um novo artigo editado de novo e de novo e DE NOVO</p>`,
-  metadata: {
-    lastModified: "2025-09-13T20:40:34.625Z",
-    views: 0
-  }
-}

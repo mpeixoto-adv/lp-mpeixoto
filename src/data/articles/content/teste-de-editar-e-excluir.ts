ad68a4e1a98e14a5c0c93f47396e8aa258835882
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-13 21:54:34 -0300

    Atualiza conteúdo: TESTE DE EDITAR E EXCLUIR - EDITADO

--- src/data/articles/content/teste-de-editar-e-excluir.ts
+++ src/data/articles/content/teste-de-editar-e-excluir.ts
@@ -1,7 +1,7 @@
 export const articleContent = {
-  content: `<p>TESTE DE EDITAR E EXCLUIR ARQUIVOS</p>`,
+  content: `<p>TESTE DE EDITAR E EXCLUIR ARQUIVOS - EDITADO</p>`,
   metadata: {
-    lastModified: "2025-09-14T00:53:02.763Z",
+    lastModified: "2025-09-14T00:54:34.021Z",
     views: 0
   }
 }

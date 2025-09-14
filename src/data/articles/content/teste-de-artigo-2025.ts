a5f5441b031d3ea5af903e716514b09564964b77
igorgcampos <31486326+igorgcampos@users.noreply.github.com>
2025-09-14 14:51:27 -0300

    Adiciona conteúdo: Teste De Artigo 2025

---
+++ src/data/articles/content/teste-de-artigo-2025.ts
@@ -0,0 +1,7 @@
+export const articleContent = {
+  content: `<p>What is Lorem Ipsum?</p><p></p><h3><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h3><p></p>`,
+  metadata: {
+    lastModified: "2025-09-14T17:51:27.453Z",
+    views: 0
+  }
+}

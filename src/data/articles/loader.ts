09da1fbb604e8dca134e74a3c31fe6a7827f89f7
Igor Gonsalves <igor.gonsalves@gmail.com>
2025-09-17 13:33:10 -0300

    Habilita cache de artigos e gera loader antes do dev

--- src/data/articles/loader.ts
+++ src/data/articles/loader.ts
@@ -3,14 +3,12 @@ import { ArticleMetadata, Article } from './types';
 import * as direitos_consumidor_compras_online from './content/direitos-consumidor-compras-online';
 import * as lgpd_o_que_sua_empresa_precisa_saber from './content/lgpd-o-que-sua-empresa-precisa-saber';
 import * as reforma_tributaria_impactos_pequenas_empresas from './content/reforma-tributaria-impactos-pequenas-empresas';
-import * as teste_de_artigo_2025 from './content/teste-de-artigo-2025';
 
 // Mapa estático de conteúdos (gerado automaticamente)
 const STATIC_CONTENT_MAP: Record<string, string> = {
     'direitos-consumidor-compras-online': direitos_consumidor_compras_online.articleContent.content,
     'lgpd-o-que-sua-empresa-precisa-saber': lgpd_o_que_sua_empresa_precisa_saber.articleContent.content,
-    'reforma-tributaria-impactos-pequenas-empresas': reforma_tributaria_impactos_pequenas_empresas.articleContent.content,
-    'teste-de-artigo-2025': teste_de_artigo_2025.articleContent.content
+    'reforma-tributaria-impactos-pequenas-empresas': reforma_tributaria_impactos_pequenas_empresas.articleContent.content
 };
 
 // SISTEMA HÍBRIDO DEFINITIVO - NÃO MODIFICAR
